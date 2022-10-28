09ad499c60baecec8cdf1bab9d416d4834ec59ac
Kvanzi <104501290+Kvanzi@users.noreply.github.com>
2022-10-28 21:05:52 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,11 +16,18 @@ window.addEventListener('scroll', () => {
 
 const headerBtn = document.querySelector('.header__btn');
 const list = document.querySelector('.header__list');
+const listItem = list.querySelectorAll('a');
 
 if (headerBtn) {
     headerBtn.addEventListener('click', () => {
         headerBtn.classList.toggle('active');
         list.classList.toggle('active');
         document.body.classList.toggle('active');
-    })
+    });
 }
+list.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+        headerBtn.classList.toggle('active')
+        list.classList.toggle('active')
+    })
+});
